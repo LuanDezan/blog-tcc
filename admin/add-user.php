3a9b50ab4bd8a88c203c46ed2e466256f43129d2
LuanDezan <112398074+LuanDezan@users.noreply.github.com>
2023-11-23 17:58:19 -0300

    Add files via upload

---
+++ admin/add-user.php
@@ -0,0 +1,57 @@
+<?php
+    include 'partials/header.php';
+
+    // get back form data if there was an error
+    $firstname = $_SESSION['add-user-data']['firstname'] ?? null;
+    $lastname = $_SESSION['add-user-data']['lastname'] ?? null;
+    $username = $_SESSION['add-user-data']['username'] ?? null;
+    $email = $_SESSION['add-user-data']['email'] ?? null;
+    $createpassword = $_SESSION['add-user-data']['createpassword'] ?? null;
+    $confirmpassword = $_SESSION['add-user-data']['confirmpassword'] ?? null;
+
+    //delete session data
+    unset($_SESSION['add-user-data']);
+?>
+
+
+<section class="form__section">
+    <div class="container form__section-container">
+        <h2>Adicionar usuário</h2>
+        <?php if(isset($_SESSION['add-user'])) : ?>
+            <div class="alert__message error">
+                <p>
+                    <?= $_SESSION['add-user'];
+                    unset($_SESSION['add-user']);
+                    ?>
+                </p>
+            </div>
+
+            <?php endif ?>
+        <form action="<?= ROOT_URL ?>admin/add-user-logic.php" enctype="multipart/form-data"
+        method="POST">
+            <input type="text" name="firstname" value="<?= $firstname ?>" placeholder="Primeiro nome">
+            <input type="text" name="lastname" value="<?= $lastname ?>" placeholder="Sobrenome">
+            <input type="text" name="username" value="<?= $username ?>" placeholder="Nome de usuário">
+            <input type="text" name="email" value="<?= $email ?>" placeholder="Email">
+            <input type="password" name="createpassword" value="<?= $createpassword ?>" placeholder="Criar senha">
+            <input type="password" name="confirmpassword" value="<?= $confirmpassword ?>" placeholder="Confirmar senha">
+            <select name="userrole">
+                <option value="0">Usuário</option>
+                <option value="2">Escritor</option>
+                <option value="1">Administrador</option>
+            </select>
+            <div class="form__control">
+                <label for="avatar">Avatar do usuário</label>
+                <input type="file" name="avatar" id="avatar">
+            </div>
+            <button type="submit" name="submit" class="btn">Adicionar usuário</button>
+        </form>
+    </div>
+</section>
+
+
+
+
+<?php
+    include '../partials/footer.php';
+?>
